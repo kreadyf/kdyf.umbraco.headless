858c18ff824c79bb0a2e610813060d1fd51c0312
Florian Dürrbaum | treesult <florian.duerrbaum@treesult.ch>
2020-06-12 10:06:37 -0400

    Naming

--- kdyf.umbraco8.headless/Properties/AssemblyInfo.cs
+++ kdyf.umbraco8.headless/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("kdyf.umbraco8.headleass")]
+[assembly: AssemblyTitle("kdyf.umbraco8.headless")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("kdyf.umbraco8.headleass")]
+[assembly: AssemblyProduct("kdyf.umbraco8.headless")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
